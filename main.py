c7f5a4c6f7d74bf289ab57ab0cd859b486fedfbc
Calum Courtney <Courtneycd@cardiff.ac.uk>
2025-01-29 14:50:30 +0000

    send email/recieve message fully functional

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import threading
+
 import boto3
 import json
 import os
@@ -20,28 +22,59 @@ RECIPIENT_EMAIL = os.getenv('RECIPIENT_EMAIL')
 
 
 def process_sqs_p3_message():
+    while True:
+        try:
+            # Receive the message from the SQS queue
+            response = sqs_client.receive_message(
+                QueueUrl=QUEUE_URL,
+                MaxNumberOfMessages=1,
+                WaitTimeSeconds=10
+            )
 
-    title = "Broken frontend"
-    description = "The frontend of the website is broken"
+            messages = response.get('Messages', [])
 
-    email_template = f"Hello,\n\nPlease see below the following high priority bug report:\n\nTitle: {title}\n\nDescription: {description}"
+            if not messages:
+                continue
 
-    ses_client.send_email(
-        Source=SES_EMAIL,
-        Destination = {'ToAddresses': [RECIPIENT_EMAIL]},
-        Message = {
-            'Subject': {
-                'Data': f"High Priority Bug: {title}"
-            },
-            'Body': {
-                'Text':{
-                    'Data': email_template
+            message = messages[0]
+            receipt_handle = message['ReceiptHandle']
+            message_body = json.loads(message['Body'])
+            print(f"Received message: {message_body}")
+
+
+            title = message_body.get('title')
+            description = message_body.get('description')
+
+            email_template = f"Hello,\n\nPlease see below the following high priority bug report:\n\nTitle: {title}\n\nDescription: {description}"
+
+            ses_client.send_email(
+                Source=SES_EMAIL,
+                Destination = {'ToAddresses': [RECIPIENT_EMAIL]},
+                Message = {
+                    'Subject': {
+                        'Data': f"High Priority Bug: {title}"
+                    },
+                    'Body': {
+                        'Text':{
+                            'Data': email_template
+                        }
+                    }
                 }
-            }
-        }
-    )
+            )
+
+            # Delete the message from the SQS queue after processing
+            sqs_client.delete_message(
+                QueueUrl=QUEUE_URL,
+                ReceiptHandle=receipt_handle
+            )
+            print(f"Message deleted from SQS queue: {receipt_handle}")
+
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
 
 
 if __name__ == '__main__':
-    process_sqs_p3_message()
+    # Run the function in a separate thread
+    threading.Thread(target=process_sqs_p3_message, daemon=True).start()
+
     app.run(debug=False, port=5003)
